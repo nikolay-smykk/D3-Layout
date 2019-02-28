b6fffcebcd4650a6f52b95ee0b44c7f9b07bc003
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2019-02-28 20:58:45 +0300

    Setting up our line chart

--- first-test/chart.js
+++ first-test/chart.js
@@ -2,7 +2,11 @@ import * as d3 from "d3";
 
 async function drawLineChart() {
   const dataset = await d3.json("./data/my_weather_data.json");
-  console.table(dataset[0]);
+
+  const yAccessor = d => d.temperatureMax
+  const dateParser = d3.timeParse("%Y-%m-%d")
+  const xAccessor = d => dateParser(d.date)
+
 }
 
 drawLineChart()
