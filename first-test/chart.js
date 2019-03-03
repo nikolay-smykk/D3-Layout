dec874243b6d991eafb0ed9aba542586ef30dfc6
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2019-03-03 20:59:49 +0300

    Drawing our chart

--- first-test/chart.js
+++ first-test/chart.js
@@ -7,6 +7,25 @@ async function drawLineChart() {
   const dateParser = d3.timeParse("%Y-%m-%d")
   const xAccessor = d => dateParser(d.date)
 
+  // 2. Create chart dimensions
+
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
 }
 
 drawLineChart()
