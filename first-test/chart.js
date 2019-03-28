535440c5d69b08fd30866796fb3e126d397e4049
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2019-03-28 21:12:04 +0300

    add ignore

--- first-test/chart.js
+++ first-test/chart.js
@@ -1,11 +1,11 @@
-import * as d3 from "d3";
+import * as d3 from 'd3';
 
 async function drawLineChart() {
-  const dataset = await d3.json("./data/my_weather_data.json");
+  const dataset = await d3.json('./data/my_weather_data.json');
 
-  const yAccessor = d => d.temperatureMax
-  const dateParser = d3.timeParse("%Y-%m-%d")
-  const xAccessor = d => dateParser(d.date)
+  const yAccessor = (d) => d.temperatureMax;
+  const dateParser = d3.timeParse('%Y-%m-%d');
+  const xAccessor = (d) => dateParser(d.date);
 
   // 2. Create chart dimensions
 
@@ -18,18 +18,15 @@ async function drawLineChart() {
       bottom: 40,
       left: 60,
     },
-  }
-  dimensions.boundedWidth = dimensions.width
-    - dimensions.margin.left
-    - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height
-    - dimensions.margin.top
-    - dimensions.margin.bottom
+  };
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
   // 3. Draw canvas
 
-  const wrapper = d3.select("#wrapper")
-
+  const wrapper = d3.select('#wrapper');
 }
 
-drawLineChart()
+drawLineChart();
