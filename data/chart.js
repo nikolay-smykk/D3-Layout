4f09fc889025129ecc129be001983bfb6df83b6d
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2019-05-11 22:19:52 +0300

    Create scales

--- data/chart.js
+++ data/chart.js
@@ -6,6 +6,7 @@ async function drawBars() {
   const dataset = await d3.json("./data/my_weather_data.json")
 
   const xAccessor = d => d.humidity
+  const yAccessor = d => d.length
 
   // 2. Create chart dimensions
 
@@ -26,6 +27,39 @@ async function drawBars() {
   dimensions.boundedHeight = dimensions.height
     - dimensions.margin.top
     - dimensions.margin.bottom
+
+  // 3. Draw canvas
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+
+  const bounds = wrapper.append("g")
+      .style("transform", `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
+
+  // 4. Create scales
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice()
+
+  const binsGenerator = d3.bin()
+    .domain(xScale.domain())
+    .value(xAccessor)
+    .thresholds(12)
+
+  const bins = binsGenerator(dataset)
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(bins, yAccessor)])
+    .range([dimensions.boundedHeight, 0])
+    .nice()
 
 }
 drawBars()
