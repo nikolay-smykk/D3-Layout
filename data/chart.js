f723a7c6e910b14afca2888277c1a8d4db0c5889
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2019-05-22 22:20:35 +0300

    Draw data

--- data/chart.js
+++ data/chart.js
@@ -61,5 +61,25 @@ async function drawBars() {
     .range([dimensions.boundedHeight, 0])
     .nice()
 
+  // 5. Draw data
+  const binsGroup = bounds.append("g")
+
+  const binGroups = binsGroup.selectAll("g")
+    .data(bins)
+    .join("g")
+
+  const barPadding = 1
+  const barRects = binGroups.append("rect")
+      .attr("x", d => xScale(d.x0) + barPadding / 2)
+      .attr("y", d => yScale(yAccessor(d)))
+      .attr("width", d => d3.max([
+        0,
+        xScale(d.x1) - xScale(d.x0) - barPadding
+      ]))
+      .attr("height", d => dimensions.boundedHeight
+        - yScale(yAccessor(d))
+      )
+      .attr("fill", "cornflowerblue")
+
 }
 drawBars()
